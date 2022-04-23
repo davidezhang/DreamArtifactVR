b9f718dcc7a532d789fec564e7ef1b2df5d8b626
Davide Zhang <11183058+davidezhang@users.noreply.github.com>
2022-04-23 17:09:50 -0400

    import Neurosity SDK

---
+++ Assets/Scripts/Notion-Unity/Handlers/BrainwavesRawHandler.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using UnityEngine;
+
+namespace Notion.Unity
+{
+    public class BrainwavesRawHandler : IMetricHandler
+    {
+        public Metrics Metric => Metrics.Brainwaves;
+        public string Label => "raw";
+
+        private readonly StringBuilder _builder;
+
+        public BrainwavesRawHandler()
+        {
+            _builder = new StringBuilder();
+        }
+
+        public void Handle(string metricData)
+        {
+            Epoch epoch = JsonConvert.DeserializeObject<Epoch>(metricData);
+
+            _builder.AppendLine("Handling Raw Brainwaves")
+                .Append("Label: ").AppendLine(epoch.Label)
+                .Append("Notch Frequency: ").AppendLine(epoch.Info.NotchFrequency)
+                .Append("Sampling Rate: ").AppendLine(epoch.Info.SamplingRate.ToString())
+                .Append("Star Time: ").AppendLine(epoch.Info.StartTime.ToString())
+                .Append("Channel Names: ").AppendLine(string.Join(", ", epoch.Info.ChannelNames));
+
+            Debug.Log(_builder.ToString());
+            _builder.Clear();
+
+            PrintData(epoch.Data);
+        }
+
+        private void PrintData(decimal[][] arr)
+        {
+            foreach (decimal[] item in arr)
+            {
+                foreach (decimal dat in item)
+                {
+                    Debug.Log(dat.ToString());
+                }
+            }
+        }
+    }
+}
